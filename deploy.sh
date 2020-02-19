bbf66e1426eb373c7c15c38b43388748ea2fed96
yelog <jaytp@qq.com>
2020-02-19 15:03:07 +0800

    add: 添加服务器发包脚本

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-scp target/layui-soul-table-java.war root@server63:/usr/dev/layui-soul-table/package
-ssh root@server63 'sh /usr/dev/layui-soul-table/script/deploy.sh'
+tomcat_path='/usr/local/apache-tomcat-8.5.27'
+package_path='/usr/dev/layui-soul-table/package/*.war'
+TomcatID=$(ps -ef |grep tomcat |grep -w $tomcat_path|grep -v 'grep'|awk '{print $2}')
+if [ -n "$TomcatID" ];
+
+       then
+                echo "$TomcatID tomcat is starting ................."
+                kill -9 $TomcatID
+                echo 'stop tomcat'
+        else
+                echo "tomcat not start ============================="
+fi
+echo 'rewrite application============='
+rm -rf $tomcat_path/webapps/ROOT/*
+echo 'start unzip and move package to tomcat folder'
+unzip -oq $package_path -d $tomcat_path/webapps/ROOT/
+sh $tomcat_path/bin/startup.sh
